d0d3f1a0e48318f3df21838b5e0de92a67ac07fa
sayaka.ito <>
2014-05-29 16:33:31 +0900

    preferencesの追加

--- src/jp/co/headwaters/mrb/view/CEditText.java
+++ src/jp/co/headwaters/mrb/view/CEditText.java
@@ -1,55 +1,56 @@
 package jp.co.headwaters.mrb.view;
 
-import java.util.Map;
-import java.util.Set;
-
 import jp.co.headwaters.mrb.R;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
 import android.view.Gravity;
 import android.widget.EditText;
 
 public class CEditText extends EditText {
 
-    String key = null;
-    SharedPreferences sharedPreferences;
-    Context context;
-    Editor prefsPrivateEditor;
-    
+    private SharedPreferences sharedPreferences;
+    private String preferenceskey = null;
+    private Editor prefsPrivateEditor;
+
+    /**
+     * sharedPreferencesの取得
+     * @param context
+     */
     public CEditText(Context context) {
         super(context);
         setBackgroundResource(R.drawable.bg_exit_text);
         setGravity(Gravity.CENTER);
 
-        this.context = context;
-        
-        //sharedPreferencesの取得
-        sharedPreferences = context.getSharedPreferences("preferencesName",
-                Context.MODE_PRIVATE);
+        sharedPreferences = context.getSharedPreferences("preferencesName", Context.MODE_PRIVATE);
         prefsPrivateEditor = sharedPreferences.edit();
     }
 
-    //Preferencesのキー文字列を設定する
+    /**
+     * Preferencesのキー文字列を設定する
+     * 
+     * @param key
+     */
     public void setKey(String key) {
-        this.key = key;
+        this.preferenceskey = key;
     }
 
-    //登録データを登録する
+    /**
+     * 登録データを登録する
+     */
     public void save() {
-        if (key != null) {
-            prefsPrivateEditor.putString(key, this.getText().toString());
+        if (preferenceskey != null) {
+            prefsPrivateEditor.putString(preferenceskey, this.getText().toString());
             prefsPrivateEditor.commit();
         }
     }
 
-    //登録データを読み込む
+    /**
+     * 登録データを読み込む
+     */
     public void load() {
-        if (key != null) {
-            String str = sharedPreferences.getString(key, "");
+        if (preferenceskey != null) {
+            String str = sharedPreferences.getString(preferenceskey, "");
             this.setText(str);
         }
     }
