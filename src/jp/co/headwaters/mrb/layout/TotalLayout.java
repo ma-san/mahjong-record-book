69187b19dde2591e6a3fdb4e226226a3a4b84ede
yuji-matsuda <maxako.maxako@gmail.com>
2014-06-12 11:41:25 +0900

    Update TotalLayout.java

--- src/jp/co/headwaters/mrb/layout/TotalLayout.java
+++ src/jp/co/headwaters/mrb/layout/TotalLayout.java
@@ -10,7 +10,8 @@ import android.widget.LinearLayout;
 
 public class TotalLayout extends LinearLayout {
     
-	private CTextView[] playerAmount;
+	/** 得点合計 */    
+	private CTextView[] playerTotal;
     
 	public TotalLayout(Context context) {
 		super(context);
@@ -22,8 +23,8 @@ public class TotalLayout extends LinearLayout {
 		total.setText(R.string.total);
 		addView(total, LayoutParamsUtil.createWidthWeightParams(1));
         
-		playerAmount = new CTextView[Constant.SUPPORT_NUMBER];
-		for (CTextView tv: playerAmount) {
+		playerTotal = new CTextView[Constant.SUPPORT_NUMBER];
+		for (CTextView tv: playerTotal) {
 			tv = new CTextView(getContext());
 			addView(tv, LayoutParamsUtil.createWidthWeightParams(2));
 		}
@@ -32,34 +33,5 @@ public class TotalLayout extends LinearLayout {
 		point.setText(R.string.point);
 		addView(point, LayoutParamsUtil.createWidthWeightParams(1));
 	}
-	
-	/**
-	 * インスタンス変数のplayerAmountから
-	 * 引数インデックスの配列位置にあるCTextViewの設定値を数値に変換して返却する。
-	 *
-	 * @param index インデックス
-	 * @return plyaerAmoountの設定値
-	 */
-	public int getPlayerAmount(int index) {
-		
-		int result = 0;
-		
-		try {
-			result = Integer.parseInt(playerAmount[index].getText().toString());
-		} catch (NumberFormatException e) {
-			result = 0;
-		}
-		
-		return result;
-	}
-	
-	public void setPlayerAmount(int index, int value) {
-        
-		if(value >= 0) {
-			playerAmount[index].setTextColor(Color.BLACK);
-		}
-		else {
-			playerAmount[index].setTextColor(Color.RED);
-		}
-	}
+
 }
