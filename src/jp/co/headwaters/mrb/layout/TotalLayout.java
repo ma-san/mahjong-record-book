1939cbe25477bb4643b4bad97cd483ae3ef4df1b
kanna ushiki <kanna@kanna-no-MacBook-Air.local>
2014-06-14 21:54:07 +0700

    updata TotalLayout

--- src/jp/co/headwaters/mrb/layout/TotalLayout.java
+++ src/jp/co/headwaters/mrb/layout/TotalLayout.java
@@ -10,7 +10,7 @@ import android.widget.LinearLayout;
 
 public class TotalLayout extends LinearLayout {
     
-	/** 得点合計 */    
+	/** 得点合計 */
 	private CTextView[] playerTotal;
     
 	public TotalLayout(Context context) {
@@ -33,7 +33,7 @@ public class TotalLayout extends LinearLayout {
 		point.setText(R.string.point);
 		addView(point, LayoutParamsUtil.createWidthWeightParams(1));
 	}
-
+    
 	/**
 	 * 得点合計を取得する。
 	 * インデックスの位置にある得点合計を返却する。
@@ -42,18 +42,18 @@ public class TotalLayout extends LinearLayout {
 	 * @return 得点合計
 	 */
 	public int getPlayerTotal(int index) {
-
+        
 		int result = 0;
-
+        
 		try {
 			result = Integer.parseInt(playerTotal[index].getText().toString());
 		} catch (NumberFormatException e) {
 			result = 0;
 		}
-
+        
 		return result;
 	}
-
+    
 	/**
 	 * 得点合計を設定する。
 	 * インデックスの位置にある得点合計を設定する。
@@ -62,7 +62,11 @@ public class TotalLayout extends LinearLayout {
 	 * @param value 得点合計
 	 */
 	public void setPlayerTotal(int index, int value) {
-
+		
+		
+		playerTotal[index].setText(Integer.toString(value));
+        
+        
 		if(value >= 0) {
 			playerTotal[index].setTextColor(Color.BLACK);
 		}
@@ -70,5 +74,8 @@ public class TotalLayout extends LinearLayout {
 			playerTotal[index].setTextColor(Color.RED);
 		}
 	}
-
+    
 }
+
+
+
